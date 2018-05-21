0eb35651c9cd4a8ee1ea7034f61744f127a2503c
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-05-21 17:56:10 +0100

    updated cntk 2.5.1

--- CNTK/src/execution/ImagenetEstimatorCNTK.py
+++ CNTK/src/execution/ImagenetEstimatorCNTK.py
@@ -21,94 +21,96 @@ from resnet_models import *
 import cntk.io.transforms as xforms
 
 # Paths relative to current python file.
-abs_path = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(abs_path, "..", "..", "..", "DataSets", "ImageNet")
+abs_path   = os.path.dirname(os.path.abspath(__file__))
+data_path  = os.path.join(abs_path, "..", "..", "..", "DataSets", "ImageNet")
 model_path = os.path.join(abs_path, "Models")
 
 # model dimensions
 image_height = 224
-image_width = 224
-num_channels = 3  # RGB
-num_classes = 1000
-model_name = "ResNet_ImageNet.model"
-
+image_width  = 224
+num_channels = 3 # RGB
+num_classes  = 1000
+model_name   = "ResNet_ImageNet.model"
 
 def create_image_mb_source(map_file, mean_file, train, total_number_of_samples):
     if not os.path.exists(map_file) or not os.path.exists(mean_file):
-        raise RuntimeError("File '%s' or '%s' does not exist." %
-                           (map_file, mean_file))
+        raise RuntimeError("File '%s' or '%s' does not exist." % (map_file, mean_file))
 
     # transformation pipeline for the features has jitter/crop only when training
     transforms = []
     if train:
         transforms += [
-            xforms.crop(crop_type='randomarea', area_ratio=(0.08, 1.0), aspect_ratio=(
-                0.75, 1.3333), jitter_type='uniratio'),  # train uses jitter
-            xforms.color(brightness_radius=0.4,
-                         contrast_radius=0.4, saturation_radius=0.4)
+            xforms.crop(crop_type='randomarea', area_ratio=(0.08, 1.0), aspect_ratio=(0.75, 1.3333), jitter_type='uniratio'), # train uses jitter
+            xforms.color(brightness_radius=0.4, contrast_radius=0.4, saturation_radius=0.4)
         ]
     else:
         transforms += [
-            # test has no jitter
-            C.io.transforms.crop(crop_type='center', side_ratio=0.875)
+            C.io.transforms.crop(crop_type='center', side_ratio=0.875) # test has no jitter
         ]
 
     transforms += [
-        xforms.scale(width=image_width, height=image_height,
-                     channels=num_channels, interpolations='cubic'),
+        xforms.scale(width=image_width, height=image_height, channels=num_channels, interpolations='cubic'),
         xforms.mean(mean_file)
     ]
 
     # deserializer
     return C.io.MinibatchSource(
         C.io.ImageDeserializer(map_file, C.io.StreamDefs(
-            # 1st col in mapfile referred to as 'image'
-            features=C.io.StreamDef(field='image', transforms=transforms),
+            features=C.io.StreamDef(field='image', transforms=transforms), # 1st col in mapfile referred to as 'image'
             labels=C.io.StreamDef(field='label', shape=num_classes))),     # and second as 'label'
         randomize=train,
         max_samples=total_number_of_samples,
         multithreaded_deserializer=True)
 
 # Create network
-
-
-def create_resnet_network(network_name):
+def create_resnet_network(network_name, fp16):
     # Input variables denoting the features and label data
     input_var = C.input_variable((num_channels, image_height, image_width))
     label_var = C.input_variable((num_classes))
 
-    stride1x1 = (1, 1)
-    stride3x3 = (2, 2)
-
-    # create model, and configure learning parameters
-    if network_name == 'resnet18':
-        z = create_imagenet_model_basic(input_var, [2, 1, 1, 2], num_classes)
-    elif network_name == 'resnet34':
-        z = create_imagenet_model_basic(input_var, [3, 3, 5, 2], num_classes)
-    elif network_name == 'resnet50':
-        z = create_imagenet_model_bottleneck(
-            input_var, [2, 3, 5, 2], num_classes, stride1x1, stride3x3)
-    elif network_name == 'resnet101':
-        z = create_imagenet_model_bottleneck(
-            input_var, [2, 3, 22, 2], num_classes, stride1x1, stride3x3)
-    elif network_name == 'resnet152':
-        z = create_imagenet_model_bottleneck(
-            input_var, [2, 7, 35, 2], num_classes, stride1x1, stride3x3)
+    dtype = np.float16 if fp16 else np.float32
+    if fp16:
+        graph_input = C.cast(input_var, dtype=np.float16)
+        graph_label = C.cast(label_var, dtype=np.float16)
     else:
-        return RuntimeError("Unknown model name!")
+        graph_input = input_var
+        graph_label = label_var
 
-    # loss and metric
-    ce = cross_entropy_with_softmax(z, label_var)
-    errs = classification_error(z, label_var, topN=1)
-    top5Errs = classification_error(z, label_var, topN=5)
+    with C.default_options(dtype=dtype):
+        stride1x1 = (1, 1)
+        stride3x3 = (2, 2)
+
+        # create model, and configure learning parameters
+        if network_name == 'resnet18':
+            z = create_imagenet_model_basic(graph_input, [2, 1, 1, 2], num_classes)
+        elif network_name == 'resnet34':
+            z = create_imagenet_model_basic(graph_input, [3, 3, 5, 2], num_classes)
+        elif network_name == 'resnet50':
+            z = create_imagenet_model_bottleneck(graph_input, [2, 3, 5, 2], num_classes, stride1x1, stride3x3)
+        elif network_name == 'resnet101':
+            z = create_imagenet_model_bottleneck(graph_input, [2, 3, 22, 2], num_classes, stride1x1, stride3x3)
+        elif network_name == 'resnet152':
+            z = create_imagenet_model_bottleneck(graph_input, [2, 7, 35, 2], num_classes, stride1x1, stride3x3)
+        else:
+            return RuntimeError("Unknown model name!")
+
+        # loss and metric
+        ce = cross_entropy_with_softmax(z, graph_label)
+        errs = classification_error(z, graph_label, topN=1)
+        top5Errs = classification_error(z, graph_label, topN=5)
+
+    if fp16:
+        ce = C.cast(ce, dtype=np.float32)
+        errs = C.cast(errs, dtype=np.float32)
+        top5Errs = C.cast(top5Errs, dtype=np.float32)
 
     return {
-        'name': network_name,
+        'name' : network_name,
         'feature': input_var,
         'label': label_var,
-        'ce': ce,
-        'errs': errs,
-        'top5Errs': top5Errs,
+        'ce' : ce,
+        'errs' : errs,
+        'top5Errs' : top5Errs,
         'output': z
     }
 
@@ -124,8 +126,7 @@ def create_trainer(network, minibatch_size, epoch_size, num_quantization_bits, b
             lr_per_mb[i] *= minibatch_size / 256
 
     # Set learning parameters
-    lr_schedule = learning_rate_schedule(
-        lr_per_mb, epoch_size=epoch_size, unit=UnitType.minibatch)
+    lr_schedule = learning_rate_schedule(lr_per_mb, epoch_size=epoch_size, unit=UnitType.minibatch)
     mm_schedule = momentum_schedule(0.9)
 
     local_learner = nesterov(network['output'].parameters, lr_schedule, mm_schedule,
@@ -133,21 +134,16 @@ def create_trainer(network, minibatch_size, epoch_size, num_quantization_bits, b
 
     # learner object
     if block_size != None and num_quantization_bits != 32:
-        raise RuntimeError(
-            "Block momentum cannot be used with quantization, please remove quantized_bits option.")
+        raise RuntimeError("Block momentum cannot be used with quantization, please remove quantized_bits option.")
 
     if block_size != None:
-        learner = block_momentum_distributed_learner(
-            local_learner, block_size=block_size)
+        learner = block_momentum_distributed_learner(local_learner, block_size=block_size)
     else:
-        learner = data_parallel_distributed_learner(
-            local_learner, num_quantization_bits=num_quantization_bits, distributed_after=warm_up)
+        learner = data_parallel_distributed_learner(local_learner, num_quantization_bits=num_quantization_bits, distributed_after=warm_up)
 
     return Trainer(network['output'], (network['ce'], network['errs']), learner, progress_printer)
 
 # Train and test
-
-
 def train_and_test(network, trainer, train_source, test_source, minibatch_size, epoch_size, restore=True, profiling=False):
 
     # define mapping from intput streams to network inputs
@@ -165,8 +161,7 @@ def train_and_test(network, trainer, train_source, test_source, minibatch_size, 
         mb_size=minibatch_size,
         model_inputs_to_streams=input_map,
         progress_frequency=epoch_size,
-        checkpoint_config=CheckpointConfig(frequency=epoch_size, filename=os.path.join(
-            model_path, model_name), restore=restore),
+        checkpoint_config=CheckpointConfig(frequency=epoch_size, filename=os.path.join(model_path, model_name), restore=restore),
         test_config=TestConfig(test_source, minibatch_size)
     ).train()
 
@@ -174,10 +169,8 @@ def train_and_test(network, trainer, train_source, test_source, minibatch_size, 
         stop_profiler()
 
 # Train and evaluate the network.
-
-
-def resnet_imagenet(train_data, test_data, mean_data, network_name, epoch_size, num_quantization_bits=32, block_size=None, warm_up=0,
-                    max_epochs=90, restore=True, log_to_file=None, num_mbs_per_log=100, gen_heartbeat=False, scale_up=False, profiling=False):
+def resnet_imagenet(train_data, test_data, mean_data, network_name, epoch_size, num_quantization_bits=32, block_size=None, warm_up=0, 
+                    max_epochs=90, restore=True, log_to_file=None, num_mbs_per_log=100, gen_heartbeat=False, scale_up=False, profiling=False, fp16=False):
 
     set_computation_network_trace_level(0)
 
@@ -195,48 +188,31 @@ def resnet_imagenet(train_data, test_data, mean_data, network_name, epoch_size, 
         gen_heartbeat=gen_heartbeat,
         num_epochs=max_epochs)
 
-    network = create_resnet_network(network_name)
-    trainer = create_trainer(network, minibatch_size, epoch_size,
-                             num_quantization_bits, block_size, warm_up, progress_printer)
-    train_source = create_image_mb_source(
-        train_data, mean_data, train=True, total_number_of_samples=max_epochs * epoch_size)
-    test_source = create_image_mb_source(
-        test_data, mean_data, train=False, total_number_of_samples=C.io.FULL_DATA_SWEEP)
-    train_and_test(network, trainer, train_source, test_source,
-                   minibatch_size, epoch_size, restore, profiling)
+    network = create_resnet_network(network_name, fp16)
+    trainer = create_trainer(network, minibatch_size, epoch_size, num_quantization_bits, block_size, warm_up, progress_printer)
+    train_source = create_image_mb_source(train_data, mean_data, train=True, total_number_of_samples=max_epochs * epoch_size)
+    test_source = create_image_mb_source(test_data, mean_data, train=False, total_number_of_samples=C.io.FULL_DATA_SWEEP)
+    train_and_test(network, trainer, train_source, test_source, minibatch_size, epoch_size, restore, profiling)
 
 
-if __name__ == '__main__':
-    data_path = os.path.join(abs_path, "..", "..",
-                             "..", "DataSets", "ImageNet")
+if __name__=='__main__':
+    data_path = os.path.join(abs_path, "..", "..", "..", "DataSets", "ImageNet")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--network', help='network type, resnet18, 34, 50, 101, 152',
-                        required=False, default='resnet50')
-    parser.add_argument('-s', '--scale_up', help='scale up minibatch size with #workers for better parallelism',
-                        type=bool, required=False, default='True')
-    parser.add_argument('-datadir', '--datadir',
-                        help='Data directory where the ImageNet dataset is located', required=False, default=data_path)
-    parser.add_argument('-outputdir', '--outputdir',
-                        help='Output directory for checkpoints and models', required=False, default=None)
-    parser.add_argument('-logdir', '--logdir',
-                        help='Log file', required=False, default=None)
-    parser.add_argument('-e', '--epochs', help='Total number of epochs to train',
-                        type=int, required=False, default='90')
-    parser.add_argument('-es', '--epoch_size', help='Size of epoch in samples',
-                        type=int, required=False, default='1281167')
-    parser.add_argument('-q', '--quantized_bits', help='Number of quantized bits used for gradient aggregation',
-                        type=int, required=False, default='32')
-    parser.add_argument('-b', '--block_samples', type=int,
-                        help="Number of samples per block for block momentum (BM) distributed learner (if 0 BM learner is not used)", required=False, default=None)
-    parser.add_argument('-a', '--distributed_after',
-                        help='Number of samples to train with before running distributed', type=int, required=False, default='0')
-    parser.add_argument('-r', '--restart', help='Indicating whether to restart from scratch (instead of restart from checkpoint file by default)',
-                        action='store_true', default=False)
-    parser.add_argument('-device', '--device', type=int,
-                        help="Force to run the script on a specified device", required=False, default=None)
-    parser.add_argument('-profile', '--profile',
-                        help="Turn on profiling", action='store_true', default=False)
+    parser.add_argument('-n', '--network', help='network type, resnet18, 34, 50, 101, 152', required=False, default='resnet50')
+    parser.add_argument('-s', '--scale_up', help='scale up minibatch size with #workers for better parallelism', type=bool, required=False, default='True')
+    parser.add_argument('-datadir', '--datadir', help='Data directory where the ImageNet dataset is located', required=False, default=data_path)
+    parser.add_argument('-outputdir', '--outputdir', help='Output directory for checkpoints and models', required=False, default=None)
+    parser.add_argument('-logdir', '--logdir', help='Log file', required=False, default=None)
+    parser.add_argument('-e', '--epochs', help='Total number of epochs to train', type=int, required=False, default='90')
+    parser.add_argument('-es', '--epoch_size', help='Size of epoch in samples', type=int, required=False, default='1281167')
+    parser.add_argument('-q', '--quantized_bits', help='Number of quantized bits used for gradient aggregation', type=int, required=False, default='32')
+    parser.add_argument('-b', '--block_samples', type=int, help="Number of samples per block for block momentum (BM) distributed learner (if 0 BM learner is not used)", required=False, default=None)
+    parser.add_argument('-a', '--distributed_after', help='Number of samples to train with before running distributed', type=int, required=False, default='0')
+    parser.add_argument('-r', '--restart', help='Indicating whether to restart from scratch (instead of restart from checkpoint file by default)', action='store_true', default=False)
+    parser.add_argument('-device', '--device', type=int, help="Force to run the script on a specified device", required=False, default=None)
+    parser.add_argument('-profile', '--profile', help="Turn on profiling", action='store_true', default=False)
+    parser.add_argument('-fp16', '--fp16', help="use float16", action='store_true', default=False)
 
     args = vars(parser.parse_args())
 
@@ -253,7 +229,7 @@ if __name__ == '__main__':
     if not os.path.isdir(data_path):
         raise RuntimeError("Directory %s does not exist" % data_path)
 
-    mean_data = os.path.join(abs_path, 'ImageNet1K_mean.xml')
+    mean_data = os.path.join(data_path, 'ImageNet1K_mean.xml')
     train_data = os.path.join(data_path, 'train_map.txt')
     test_data = os.path.join(data_path, 'val_map.txt')
 
@@ -264,8 +240,7 @@ if __name__ == '__main__':
     scale_up = bool(args['scale_up'])
 
     # Create distributed trainer factory
-    print("Start training: quantize_bit = {}, epochs = {}, distributed_after = {}".format(
-        num_quantization_bits, epochs, warm_up))
+    print("Start training: quantize_bit = {}, epochs = {}, distributed_after = {}".format(num_quantization_bits, epochs, warm_up))
 
     resnet_imagenet(train_data, test_data, mean_data,
                     network_name,
@@ -277,7 +252,8 @@ if __name__ == '__main__':
                     restore=not args['restart'],
                     scale_up=scale_up,
                     log_to_file=args['logdir'],
-                    profiling=args['profile'])
+                    profiling=args['profile'],
+                    fp16=args['fp16'])
 
     # Must call MPI finalize when process exit without exceptions
     Communicator.finalize()
--- CNTK/src/execution/resnet_models.py
+++ CNTK/src/execution/resnet_models.py
@@ -12,15 +12,10 @@ from cntk.ops import element_times, relu
 #
 # assembly components
 #
-
-
 def conv_bn(input, filter_size, num_filters, strides=(1, 1), init=he_normal(), bn_init_scale=1):
-    c = Convolution(filter_size, num_filters, activation=None,
-                    init=init, pad=True, strides=strides, bias=False)(input)
-    r = BatchNormalization(map_rank=1, normalization_time_constant=4096,
-                           use_cntk_engine=False, init_scale=bn_init_scale, disable_regularization=True)(c)
+    c = Convolution(filter_size, num_filters, activation=None, init=init, pad=True, strides=strides, bias=False)(input)
+    r = BatchNormalization(map_rank=1, normalization_time_constant=4096, use_cntk_engine=False, init_scale=bn_init_scale, disable_regularization=True)(c)
     return r
-
 
 def conv_bn_relu(input, filter_size, num_filters, strides=(1, 1), init=he_normal()):
     r = conv_bn(input, filter_size, num_filters, strides, init, 1)
@@ -29,30 +24,25 @@ def conv_bn_relu(input, filter_size, num_filters, strides=(1, 1), init=he_normal
 #
 # ResNet components
 #
-
-
 def resnet_basic(input, num_filters):
     c1 = conv_bn_relu(input, (3, 3), num_filters)
     c2 = conv_bn(c1, (3, 3), num_filters, bn_init_scale=1)
     p = c2 + input
     return relu(p)
 
-
 def resnet_basic_inc(input, num_filters, strides=(2, 2)):
     c1 = conv_bn_relu(input, (3, 3), num_filters, strides)
     c2 = conv_bn(c1, (3, 3), num_filters, bn_init_scale=1)
-    s = conv_bn(input, (1, 1), num_filters, strides)  # Shortcut
+    s = conv_bn(input, (1, 1), num_filters, strides) # Shortcut
     p = c2 + s
     return relu(p)
 
-
-def resnet_basic_stack(input, num_stack_layers, num_filters):
+def resnet_basic_stack(input, num_stack_layers, num_filters): 
     assert(num_stack_layers >= 0)
     l = input
     for _ in range(num_stack_layers):
         l = resnet_basic(l, num_filters)
     return l
-
 
 def resnet_bottleneck(input, out_num_filters, inter_out_num_filters):
     c1 = conv_bn_relu(input, (1, 1), inter_out_num_filters)
@@ -61,18 +51,16 @@ def resnet_bottleneck(input, out_num_filters, inter_out_num_filters):
     p = c3 + input
     return relu(p)
 
-
 def resnet_bottleneck_inc(input, out_num_filters, inter_out_num_filters, stride1x1, stride3x3):
     c1 = conv_bn_relu(input, (1, 1), inter_out_num_filters, strides=stride1x1)
     c2 = conv_bn_relu(c1, (3, 3), inter_out_num_filters, strides=stride3x3)
     c3 = conv_bn(c2, (1, 1), out_num_filters, bn_init_scale=0)
     stride = np.multiply(stride1x1, stride3x3)
-    s = conv_bn(input, (1, 1), out_num_filters, strides=stride)  # Shortcut
+    s = conv_bn(input, (1, 1), out_num_filters, strides=stride) # Shortcut
     p = c3 + s
     return relu(p)
 
-
-def resnet_bottleneck_stack(input, num_stack_layers, out_num_filters, inter_out_num_filters):
+def resnet_bottleneck_stack(input, num_stack_layers, out_num_filters, inter_out_num_filters): 
     assert(num_stack_layers >= 0)
     l = input
     for _ in range(num_stack_layers):
@@ -82,8 +70,6 @@ def resnet_bottleneck_stack(input, num_stack_layers, out_num_filters, inter_out_
 #
 # Defines the residual network model for classifying images
 #
-
-
 def create_cifar10_model(input, num_stack_layers, num_classes):
     c_map = [16, 32, 64]
 
@@ -100,7 +86,6 @@ def create_cifar10_model(input, num_stack_layers, num_classes):
     pool = AveragePooling(filter_shape=(8, 8), name='final_avg_pooling')(r3_2)
     z = Dense(num_classes, init=normal(0.01))(pool)
     return z
-
 
 def create_imagenet_model_basic(input, num_stack_layers, num_classes):
     c_map = [64, 128, 256, 512]
@@ -123,36 +108,28 @@ def create_imagenet_model_basic(input, num_stack_layers, num_classes):
     z = Dense(num_classes, init=normal(0.01))(pool)
     return z
 
-
 def create_imagenet_model_bottleneck(input, num_stack_layers, num_classes, stride1x1, stride3x3):
     c_map = [64, 128, 256, 512, 1024, 2048]
 
     # conv1 and max pooling
     conv1 = conv_bn_relu(input, (7, 7), c_map[0], strides=(2, 2))
-    pool1 = MaxPooling((3, 3), strides=(2, 2), pad=True)(conv1)
+    pool1 = MaxPooling((3,3), strides=(2,2), pad=True)(conv1)
 
     # conv2_x
     r2_1 = resnet_bottleneck_inc(pool1, c_map[2], c_map[0], (1, 1), (1, 1))
-    r2_2 = resnet_bottleneck_stack(
-        r2_1, num_stack_layers[0], c_map[2], c_map[0])
+    r2_2 = resnet_bottleneck_stack(r2_1, num_stack_layers[0], c_map[2], c_map[0])
 
     # conv3_x
-    r3_1 = resnet_bottleneck_inc(
-        r2_2, c_map[3], c_map[1], stride1x1, stride3x3)
-    r3_2 = resnet_bottleneck_stack(
-        r3_1, num_stack_layers[1], c_map[3], c_map[1])
+    r3_1 = resnet_bottleneck_inc(r2_2, c_map[3], c_map[1], stride1x1, stride3x3)
+    r3_2 = resnet_bottleneck_stack(r3_1, num_stack_layers[1], c_map[3], c_map[1])
 
     # conv4_x
-    r4_1 = resnet_bottleneck_inc(
-        r3_2, c_map[4], c_map[2], stride1x1, stride3x3)
-    r4_2 = resnet_bottleneck_stack(
-        r4_1, num_stack_layers[2], c_map[4], c_map[2])
+    r4_1 = resnet_bottleneck_inc(r3_2, c_map[4], c_map[2], stride1x1, stride3x3)
+    r4_2 = resnet_bottleneck_stack(r4_1, num_stack_layers[2], c_map[4], c_map[2])
 
     # conv5_x
-    r5_1 = resnet_bottleneck_inc(
-        r4_2, c_map[5], c_map[3], stride1x1, stride3x3)
-    r5_2 = resnet_bottleneck_stack(
-        r5_1, num_stack_layers[3], c_map[5], c_map[3])
+    r5_1 = resnet_bottleneck_inc(r4_2, c_map[5], c_map[3], stride1x1, stride3x3)
+    r5_2 = resnet_bottleneck_stack(r5_1, num_stack_layers[3], c_map[5], c_map[3])
 
     # Global average pooling and output
     pool = AveragePooling(filter_shape=(7, 7), name='final_avg_pooling')(r5_2)
