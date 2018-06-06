b680107efb20fc41fde5dc5f9c72ae44ac8f0288
MSalvaris <msalvaris@users.noreply.github.com>
2018-06-06 12:19:12 +0100

    Fixed fake data generator

--- HorovodKeras/src/execution/data_generator.py
+++ HorovodKeras/src/execution/data_generator.py
@@ -1,8 +1,10 @@
 import numpy as np
 import keras
 import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+
+
+def _get_logger():
+    return logging.getLogger(__name__)
 
 def _create_data(batch_size, num_batches, dim, channels, seed=42):
     np.random.seed(42)
@@ -15,85 +17,7 @@ def _create_data(batch_size, num_batches, dim, channels, seed=42):
 def _create_labels(batch_size, num_batches, n_classes):
     return np.random.choice(n_classes, batch_size * num_batches)
 
-#
-# class FakeDataGenerator(keras.utils.Sequence):
-#     'Generates data for Keras'
-#
-#     def __init__(self,
-#                  batch_size=32,
-#                  num_batches=20,
-#                  dim=(224, 224),
-#                  n_channels=3,
-#                  n_classes=10,
-#                  length=1000,
-#                  shuffle=True):
-#         'Initialization'
-#         self.dim = dim
-#         self.batch_size = batch_size
-#         self.n_channels = n_channels
-#         self.n_classes = n_classes
-#         self.shuffle = shuffle
-#         self.num_batches = num_batches
-#         self._data = _create_data(self.batch_size, self.num_batches, self.dim, self.n_channels)
-#         self._labels = _create_labels(self.batch_size, self.num_batches, self.n_classes)
-#         self._indexes = np.arange(len(self._labels))
-#         self._length=length
-#         self.batch_index=0
-#         self.on_epoch_end()
-#
-#     def __len__(self):
-#         return self._length
-#
-#     def __getitem__(self, index):
-#         'Generate one batch of data'
-#         # Generate indexes of the batch
-#         indexes = self._indexes[index * self.batch_size:(index + 1) * self.batch_size]
-#
-#         # Generate data
-#         X, y = self._data_generation(indexes)
-#
-#         return X, y
-#
-#     def __iter__(self):  # pylint: disable=non-iterator-returned
-#
-#         # Needed if we want to do something like:
-#         # for x, y in data_gen.flow(...):
-#         return self
-#
-#     def __next__(self, *args, **kwargs):
-#         return self.next(*args, **kwargs)
-#
-#     def reset(self):
-#         self.batch_index = 0
-#
-#     def next(self):
-#         """For python 2.x.
-#         Returns:
-#             The next batch.
-#         """
-#         # Keeps under lock only the mechanism which advances
-#         # the indexing of each batch.
-#         with self.lock:
-#             index_array = self.batch_index
-#             self.batch_index+=1
-#             if self.batch_index>=len(self._labels):
-#                 self.reset()
-#         # The transformation of images is not under thread lock
-#         # so it can be done in parallel
-#
-#         return self[index_array]
-#
-#     def on_epoch_end(self):
-#         'Updates indexes after each epoch'
-#         if self.shuffle == True:
-#             np.random.shuffle(self._indexes)
-#
-#     def _data_generation(self, indexes):
-#         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
-#         return self._data[indexes], keras.utils.to_categorical(self._labels[indexes], num_classes=self.n_classes)
 
-
-import threading
 
 class FakeDataGenerator(keras.preprocessing.image.Iterator):
 
@@ -114,20 +38,14 @@ class FakeDataGenerator(keras.preprocessing.image.Iterator):
         self.num_batches = num_batches
         self._data = _create_data(self.batch_size, self.num_batches, self.dim, self.n_channels)
         self._labels = _create_labels(self.batch_size, self.num_batches, self.n_classes)
-        self._indexes = np.arange(len(self._labels))
-        self._length=length
         self.n = length
-        self.batch_index=0
         self.seed = 42
-
-        self.total_batches_seen = 0
-        self.lock = threading.Lock()
-        self.index_array = None
         self.translation_index = np.random.choice(len(self._labels), length)
 
 
     def _get_batches_of_transformed_samples(self, index_array):
-        logger.info('Retrieving samples')
-        logger.info(str(index_array))
+        logger = _get_logger()
+        logger.debug('Retrieving samples')
+        logger.debug(str(index_array))
         tr_index_array = self.translation_index[index_array]
         return self._data[tr_index_array], keras.utils.to_categorical(self._labels[tr_index_array], num_classes=self.n_classes)
