adb4d86c6e1624a258a527da7d40119517249fc5
MSalvaris <msalvaris@users.noreply.github.com>
2018-07-12 10:41:19 +0100

    Tyring to optimize performance of PyTorch code

--- HorovodPytorch/src/execution/ImagenetPytorchHorovod.py
+++ HorovodPytorch/src/execution/ImagenetPytorchHorovod.py
@@ -252,7 +252,7 @@ def main():
     train_sampler = torch.utils.data.distributed.DistributedSampler(
         train_dataset, num_replicas=hvd.size(), rank=hvd.rank())
 
-    kwargs = {'num_workers': 5, 'pin_memory': True}
+    kwargs = {'num_workers': 3, 'pin_memory': True}
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=_BATCHSIZE, sampler=train_sampler, **kwargs)
 
