b0f98415d67469ad7b85bb45d13e826b7f4cd837
MSalvaris <msalvaris@users.noreply.github.com>
2018-07-19 09:26:08 +0100

    Altering depth of root

--- include/build.mk
+++ include/build.mk
@@ -15,7 +15,7 @@ DATA_DIR:=/mnt/imagenet
 PWD:=$(shell pwd)
 FAKE:='False'
 FAKE_DATA_LENGTH:=1281167
-ROOT:=$(shell dirname $(shell dirname ${PWD}))
+ROOT:=$(shell dirname ${PWD})
 
 
 setup_volumes:=-v $(PWD)/src:/mnt/script \
