e10190399cca4df8083c4d64f181ade9343703e4
MSalvaris <msalvaris@users.noreply.github.com>
2018-07-31 14:28:58 +0100

    Fixes typo in path

--- experiments/local/nodeprep.sh
+++ experiments/local/nodeprep.sh
@@ -5,12 +5,16 @@ sudo cp $AZ_BATCHAI_MOUNT_ROOT/extfs/scripts/docker.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
+df -h
+
+ls $AZ_BATCHAI_MOUNT_ROOT/nfs/
+
 # Download data
 mkdir -p $AZ_BATCHAI_MOUNT_ROOT/imagenet
-rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/train.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
-rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/validation.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
-rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/train.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
-rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/validation.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/imagenet/train.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/imagenet/validation.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/imagenet/train.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/imagenet/validation.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
 cd $AZ_BATCHAI_MOUNT_ROOT/imagenet
 tar -xzf train.tar.gz
 tar -xzf validation.tar.gz
