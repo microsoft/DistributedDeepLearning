194a49ea0647c5bcff0e054c87616c91e3565bcc
MSalvaris <msalvaris@users.noreply.github.com>
2018-07-31 14:03:10 +0100

    Updates data location and data download

--- experiments/local/nodeprep.sh
+++ experiments/local/nodeprep.sh
@@ -7,7 +7,10 @@ sudo systemctl restart docker
 
 # Download data
 mkdir -p $AZ_BATCHAI_MOUNT_ROOT/imagenet
-rsync --info=progress2 -r $AZ_BATCHAI_MOUNT_ROOT/nfs/ $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/train.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/validation.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/train.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
+rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/validation.csv $AZ_BATCHAI_MOUNT_ROOT/imagenet
 cd $AZ_BATCHAI_MOUNT_ROOT/imagenet
 tar -xzf train.tar.gz
 tar -xzf validation.tar.gz
