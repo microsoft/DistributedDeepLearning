2c8ecd6d787dfc2119cd82afd076c4966a8db62c
MSalvaris <msalvaris@users.noreply.github.com>
2018-08-01 10:36:58 +0100

    Removing data download from cluster creation

--- experiments/local/downloaddata.sh
+++ experiments/local/downloaddata.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-
-# Docker config
-sudo cp $AZ_BATCHAI_MOUNT_ROOT/extfs/scripts/docker.service /lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-
-df -h
-
-ls $AZ_BATCHAI_MOUNT_ROOT/nfs/
-
 # Download data
 mkdir -p $AZ_BATCHAI_MOUNT_ROOT/imagenet
 rsync --info=progress2 $AZ_BATCHAI_MOUNT_ROOT/nfs/imagenet/train.tar.gz $AZ_BATCHAI_MOUNT_ROOT/imagenet
